98f98aaaa98e5ef335aa77adfba4b584685afb79
dinggood615 <dinggood615@users.noreply.github.com>
2026-09-04 22:57:26 +0800

    release: bump version for new icon assets

--- scripts/install-openkill.sh
+++ scripts/install-openkill.sh
@@ -4,7 +4,7 @@ set -eu
 
 REPO="dinggood615/openkill"
 PACKAGE_REF="master"
-PROJECT_VERSION="2026-1007"
+PROJECT_VERSION="2026-1008"
 ACTION=install
 PACKAGE_FILE=""
 BACKUP_DIR="/tmp/openkill-install-backup-$$"
